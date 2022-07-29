a7796e268ee94e1fff4ae581081bc5083945eda1
gnizamaaa <rhuangt@gmail.com>
2022-07-29 19:36:56 -0300

    Merge branch 'main'

--- Codificador.h
+++ Codificador.h
@@ -7,19 +7,6 @@
 
 typedef struct TabelaDeCod Tabela;
 
-Arv *FazArvoreHuffman(Listagen *listabase);
-void CompletaByteBitmap(bitmap *entrada);
-void CodificaArq(FILE *arq, Arv *Huffman, VetChar *Vetor, FILE *saida);
-static Tabela *MontandoTabela(Arv *Huffman, VetChar *vetor, int tam);
-
-/**
- * @brief Retorna o codigo referente ao caractere dado, na codificacao da arvore fornecida
- *
- * @param a  - Arvore valida, preenchida
- * @param carac - Caractere a ser codificado
- * @return bitmap* - Codigo referente ao caractere
- */
-bitmap *CodificaChar(const Arv *raiz, char carac);
-
+void Recursiva(bitmap *codificando, Arv *a, char c);
 
 #endif
