e8a933748b30504e64ace6a60017bc29d8a3b85a
Gabriel Braga <gabriel.b.ladislau@gmail.com>
2022-07-29 16:22:24 -0300

    coloquei no .h

--- Codificador.c
+++ Codificador.c
@@ -1,18 +1,5 @@
 #include "Codificador.h"
 
-Arv *FazArvoreHuffman(Listagen *listabase);
-void CompletaByteBitmap(bitmap *entrada);
-void CodificaArq(FILE *arq, Arv *Huffman, VetChar *Vetor, FILE *saida);
-static Tabela *MontandoTabela(Arv *Huffman, VetChar *vetor, int tam);
-
-/**
- * @brief Retorna o codigo referente ao caractere dado, na codificacao da arvore fornecida
- *
- * @param a  - Arvore valida, preenchida
- * @param carac - Caractere a ser codificado
- * @return bitmap* - Codigo referente ao caractere
- */
-bitmap *CodificaChar(const Arv *raiz, char carac);
 
 struct TabelaDeCod
 {
